0532ae3777108a9a2ccddf76d83e60ab2eb3cfb5
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-07-31 21:48:02 +0200

    Update Bridgeswap.tsx

--- src/bridges/Bridgeswap.tsx
+++ src/bridges/Bridgeswap.tsx
@@ -3,7 +3,7 @@ import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 const widgetConfig: WidgetConfig = {
   integrator: 'Perseus',
   fromChain: 56,
-  fromToken:'0xfa4C07636B53D868E514777B9d4005F1e9c6c40B',
+  fromToken: '0xfa4C07636B53D868E514777B9d4005F1e9c6c40B',
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
