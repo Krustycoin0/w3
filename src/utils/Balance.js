7e2931cdcf85e37facc0909d01d2302022c8de6e
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-08-01 03:35:46 +0200

    Update Balance.js

--- src/utils/Balance.js
+++ src/utils/Balance.js
@@ -9,7 +9,7 @@ export default async function Balance() {
     )
 
     const config = {
-      hostAddress: "0xD0355200111C2B21AAbC1a31552eCCDc5d4E905d",
+      hostAddress: "0x3E14dC1b13c488a8d5D310918780c983bD5982E7",
       cfaV1Address: "0x6EeE6060f715257b970700bc2656De21dEdF074C",
       idaV1Address: "0xB0aABBA4B2783A72C52956CDEF62d438ecA2d7a1"
     }
